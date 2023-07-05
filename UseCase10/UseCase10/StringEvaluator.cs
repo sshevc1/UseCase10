28c522a3465b271828691469d1b95fd67a836260
SOFTSERVE\sshevch <sshevch@softserveinc.com>
2023-07-05 10:08:45 +0300

    UC#10 Implement regex

--- UseCase10/UseCase10/StringEvaluator.cs
+++ UseCase10/UseCase10/StringEvaluator.cs
@@ -1,7 +1,14 @@
-﻿namespace UseCase10
+﻿using System.Text.RegularExpressions;
+
+namespace UseCase10
 {
     public class StringEvaluator
     {
-
+        public bool ValidateString(string input, int maxLength)
+        {
+            string regexPattern = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!""#$%&'()*+,-.\/:;<=>?@[\]^_`{|}~])[^\s]{1," + maxLength + "}$";
+            Regex regex = new Regex(regexPattern);
+            return regex.IsMatch(input);
+        }
     }
 }
