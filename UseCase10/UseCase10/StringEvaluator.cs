ab478145047e6779a6db6ea743b3e1c1e91862c0
SOFTSERVE\sshevch <sshevch@softserveinc.com>
2023-07-05 10:20:54 +0300

    UC#10 Implement regex - updated commit after conversation with GPT-4

--- UseCase10/UseCase10/StringEvaluator.cs
+++ UseCase10/UseCase10/StringEvaluator.cs
@@ -6,7 +6,7 @@ namespace UseCase10
     {
         public bool ValidateString(string input, int maxLength)
         {
-            string regexPattern = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!""#$%&'()*+,-.\/:;<=>?@[\]^_`{|}~])[^\s]{1," + maxLength + "}$";
+            string regexPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!""#$%&'()*+,-.\/:;<=>?@\\\\[\\\]^_`{|}~])(?!.*\s).{1," + maxLength + "}$";
             Regex regex = new Regex(regexPattern);
             return regex.IsMatch(input);
         }
